86d484e26d9474c1b4aa7c2fb7ae234012ccc047
vigilthomas17 <vigilthomas17@gmail.com>
2023-11-24 22:31:38 +0530

    Updated Login and Register

--- todo/form.py
+++ todo/form.py
@@ -8,10 +8,7 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-        widgets = {
-            'username': forms.TextInput(attrs={"class": "form-control "}),
-            'email': forms.EmailInput(attrs={"class": "form-control "}),
-        }
+        
 
 
 class LoginForm(forms.Form):
